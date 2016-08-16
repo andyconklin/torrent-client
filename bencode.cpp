f53fb564b0fb1becbeaa91684c00af373c23819d
Andy Conklin <dandy.andy.conklin@gmail.com>
2016-08-15 23:31:11 -0500

    Fixed whitespace

--- bencode.cpp
+++ bencode.cpp
@@ -8,8 +8,8 @@
 
 class BencodeObj {
 public:
-	virtual ~BencodeObj() {}
-	virtual void print(std::ostream &stream) const { }
+  virtual ~BencodeObj() {}
+  virtual void print(std::ostream &stream) const { }
 };
 
 class BencodeInt : public BencodeObj {
@@ -17,59 +17,59 @@ private:
   int value;
 public:
   BencodeInt(int value) : value(value) { }
-	virtual void print(std::ostream &stream) const {
-		stream << "i" << value << "e";
-	}
+  virtual void print(std::ostream &stream) const {
+    stream << "i" << value << "e";
+  }
 };
 
 class BencodeList : public BencodeObj {
 private:
   std::vector<BencodeObj *> list;
 public:
-	BencodeList(std::vector<BencodeObj *> list) : list(list) {}
-	virtual void print(std::ostream &stream) const {
-		stream << "l";
-		for (auto it = list.cbegin(); it != list.cend(); it++) {
-			(*it)->print(stream);
-		}
-		stream << "e";
-	}
-	virtual ~BencodeList() {
-		for (auto it = list.cbegin(); it != list.cend(); it++) {
-			delete *it;
-		}
-	}
+  BencodeList(std::vector<BencodeObj *> list) : list(list) {}
+  virtual void print(std::ostream &stream) const {
+    stream << "l";
+    for (auto it = list.cbegin(); it != list.cend(); it++) {
+      (*it)->print(stream);
+    }
+    stream << "e";
+  }
+  virtual ~BencodeList() {
+    for (auto it = list.cbegin(); it != list.cend(); it++) {
+      delete *it;
+    }
+  }
 };
 
 class BencodeDict : public BencodeObj {
 private:
   std::map<std::string, BencodeObj *> dict;
 public:
-	BencodeDict(std::map<std::string, BencodeObj *> dict) : dict(dict) {}
-	virtual void print(std::ostream &stream) const {
-		stream << "d";
-		for (auto it = dict.cbegin(); it != dict.cend(); it++) {
-			stream << it->first.size() << ":" << it->first;
-			it->second->print(stream);
-		}
-		stream << "e";
-	}
-	virtual ~BencodeDict() {
-		for (auto it = dict.cbegin(); it != dict.cend(); it++) {
-			delete it->second;
-		}
-	}
+  BencodeDict(std::map<std::string, BencodeObj *> dict) : dict(dict) {}
+  virtual void print(std::ostream &stream) const {
+    stream << "d";
+    for (auto it = dict.cbegin(); it != dict.cend(); it++) {
+      stream << it->first.size() << ":" << it->first;
+      it->second->print(stream);
+    }
+    stream << "e";
+  }
+  virtual ~BencodeDict() {
+    for (auto it = dict.cbegin(); it != dict.cend(); it++) {
+      delete it->second;
+    }
+  }
 };
 
 class BencodeString : public BencodeObj {
 private:
-	std::string value;
+  std::string value;
 public:
-	BencodeString(std::string value) : value(value) {}
-	std::string get_value() { return value; }
-	virtual void print(std::ostream &stream) const {
-		stream << value.size() << ":" << value;
-	}
+  BencodeString(std::string value) : value(value) {}
+  std::string get_value() { return value; }
+  virtual void print(std::ostream &stream) const {
+    stream << value.size() << ":" << value;
+  }
 };
 
 BencodeObj *BencodeDecode(std::vector<char>::const_iterator &it,
@@ -86,9 +86,9 @@ BencodeObj *BencodeDecode(std::vector<char>::const_iterator &it,
     if ((int_text[0] == '0' && int_text.size() > 1) ||
         (int_text[0] == '-' && (int_text.size() == 1 || int_text[1] == '0')))
       throw std::logic_error("Integer parsing: Leading zero");
-		return new BencodeInt(std::stoi(int_text));
+    return new BencodeInt(std::stoi(int_text));
   } else if (*it == 'l') {
-		std::vector<BencodeObj *> list;
+    std::vector<BencodeObj *> list;
     while (*(++it) != 'e') {
       list.push_back(BencodeDecode(it, it_end));
     }
@@ -105,14 +105,14 @@ BencodeObj *BencodeDecode(std::vector<char>::const_iterator &it,
       throw std::logic_error("String parsing: String too long for file size");
     std::string s(it + 1, it + 1 + len);
     it += len;
-		return new BencodeString(s);
+    return new BencodeString(s);
   } else if (*it == 'd') {
-		std::map<std::string, BencodeObj *> d;
+    std::map<std::string, BencodeObj *> d;
     while (*(++it) != 'e') {
       std::string k = (dynamic_cast<BencodeString *>(BencodeDecode(it, it_end)))->get_value();
       d.insert(make_pair(k, BencodeDecode(++it, it_end)));
     }
-		return new BencodeDict(d);
+    return new BencodeDict(d);
   } else {
     throw std::logic_error("Unrecognized bencode type");
   }
