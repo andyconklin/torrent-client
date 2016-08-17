1c62b07656bdcb4af9b989b981f213e50a2a04df
Andy Conklin <dandy.andy.conklin@gmail.com>
2016-08-17 05:34:23 -0500

    Started on the networking

--- Makefile
+++
@@ -1,2 +0,0 @@
-main: main.cpp bencode.cpp bencode.h
-  g++ main.cpp bencode.cpp -o main -std=c++11 -lcrypto
--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 #include <vector>
 #include <iomanip>
 #include <openssl/sha.h>
+#include <openssl/rand.h>
+#include <curl/curl.h>
+#include <thread>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 #include "bencode.h"
 
@@ -17,6 +23,15 @@ std::vector<char> ReadFile(std::string path) {
   return buffer;
 }
 
+void NetworkerEntry() {
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) throw std::logic_error("Unable to init socket.");
+  struct sockaddr_in my_address = {AF_INET, htons(6881), {htonl(INADDR_ANY)}, {0,0,0,0,0,0,0,0}};
+  int bindres = bind(sockfd, reinterpret_cast<const sockaddr *>(&my_address), sizeof(my_address));
+  if (bindres != 0) throw std::logic_error("Unable to bind on port 6881.");
+  std::cout << SOMAXCONN << std::endl;
+}
+
 int main(int argc, char* argv[]) {
   std::vector<char> metainfo = ReadFile(argv[1]);
   auto curr = metainfo.cbegin();
@@ -26,9 +41,24 @@ int main(int argc, char* argv[]) {
   unsigned char hash[SHA_DIGEST_LENGTH];
   SHA1(reinterpret_cast<const unsigned char *>(&metainfo[info->bounds.first]), 1 + info->bounds.second - info->bounds.first, hash);
 
-  for (int i = 0; i < SHA_DIGEST_LENGTH; i++) {
-    std::cout << std::hex << std::setfill('0') << std::setw(2) << (int)(hash[i]);
-  }
-  std::cout << std::endl;
+  CURL *curl = curl_easy_init();
+  if (!curl) throw std::logic_error("curl_easy_init() failed");
+
+  char *escaped = curl_easy_escape(curl, reinterpret_cast<const char *>(hash), 20);
+  std::cout << escaped << std::endl;
+  curl_free(escaped);
+
+  unsigned char randbytes[20];
+  RAND_bytes(randbytes, sizeof(randbytes));
+
+  escaped = curl_easy_escape(curl, reinterpret_cast<const char *>(randbytes), 20);
+  std::cout << escaped << std::endl;
+  curl_free(escaped);
+
+  curl_easy_cleanup(curl);
+
+  std::thread networker(NetworkerEntry);
+  networker.join();
+
   return 0;
 }
