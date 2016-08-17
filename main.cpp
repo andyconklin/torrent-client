c3c7bea7c6ae29e0dd1450d553a2552f6bfe717f
Andy Conklin <dandy.andy.conklin@gmail.com>
2016-08-17 06:40:44 -0500

    Listening ought to work now

--- main.cpp
+++ main.cpp
@@ -9,8 +9,11 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/select.h>
 
 #include "bencode.h"
+
+#define MAX_CONNECTIONS 10
 
 std::vector<char> ReadFile(std::string path) {
   std::ifstream file(path, std::ios::binary | std::ios::ate);
@@ -24,12 +27,41 @@ std::vector<char> ReadFile(std::string path) {
 }
 
 void NetworkerEntry() {
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd < 0) throw std::logic_error("Unable to init socket.");
-  struct sockaddr_in my_address = {AF_INET, htons(6881), {htonl(INADDR_ANY)}, {0,0,0,0,0,0,0,0}};
-  int bindres = bind(sockfd, reinterpret_cast<const sockaddr *>(&my_address), sizeof(my_address));
-  if (bindres != 0) throw std::logic_error("Unable to bind on port 6881.");
-  std::cout << SOMAXCONN << std::endl;
+  /* http://stackoverflow.com/questions/2284428 */
+  fd_set fds;
+  struct timeval timeout;
+  int max_fd = 0;
+  std::vector<int> connected;
+
+  int my_socket = socket(AF_INET, SOCK_STREAM, 0);
+  if (my_socket < 0) throw std::logic_error("Unable to init socket.");
+  struct sockaddr_in my_address = {AF_INET, htons(6881),
+      {htonl(INADDR_ANY)}, {0,0,0,0,0,0,0,0}};
+  if (bind(my_socket, reinterpret_cast<const sockaddr *>(&my_address),
+      sizeof(my_address)) != 0)
+    throw std::logic_error("Unable to bind on port 6881.");
+  if (listen(my_socket, MAX_CONNECTIONS) < 0)
+    throw std::logic_error("Listen failed.");
+  while (true) {
+    FD_ZERO(&fds);
+    FD_SET(my_socket, &fds);
+    max_fd = (max_fd > my_socket) ? max_fd : my_socket + 1;
+    for (auto it = connected.cbegin(); it != connected.cend(); it++) {
+      FD_SET(*it, &fds);
+      max_fd = (max_fd > *it) ? max_fd : *it + 1;
+    }
+    timeout.tv_sec = 2; timeout.tv_usec = 0;
+    if (select(max_fd, &fds, NULL, NULL, &timeout) > 0) {
+      if (FD_ISSET(my_socket, &fds)) {
+        connected.push_back(accept(my_socket, NULL, NULL));
+      }
+      for (auto it = connected.cbegin(); it != connected.cend(); it++) {
+        if (FD_ISSET(*it, &fds)) {
+          /* peer has something to say */
+        }
+      }
+    }
+  }
 }
 
 int main(int argc, char* argv[]) {
